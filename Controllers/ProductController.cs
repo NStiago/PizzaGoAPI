106fd57fc2f8d1d5cb3bdfe1dbf60f7e0a2da8e9
Sementiago <Sementiago@DESKTOP-GF1UAA5>
2023-06-17 01:22:23 +0700

    Создан класс Product

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -3,14 +3,15 @@
 namespace PizzaGoAPI.Controllers
 {
     [ApiController]
+    [Route("api/pizza")]
     public class ProductController : ControllerBase
     {
-        [HttpGet("api/pizza")]
+        [HttpGet]
         public IActionResult GetProducts()
         {
             return new JsonResult(new List<object>
             {
-                new {id =1, Name="Pepperoni"},
+                new {id=1, Name="Pepperoni"},
                 new {id=2, Name="Pizza with Pineapple"}
             });
         }
