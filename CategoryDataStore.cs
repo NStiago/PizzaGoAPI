730c61d9bb7c8349e60c669e3e9b57f2e703e3f1
Sementiago <Sementiago@DESKTOP-GF1UAA5>
2023-06-21 10:07:11 +0700

    Добавлен класс с данными о категориях

---
+++ CategoryDataStore.cs
@@ -0,0 +1,32 @@
+﻿using PizzaGoAPI.Models;
+
+namespace PizzaGoAPI
+{
+    public class CategoryDataStore
+    {
+        public List<Category> Categories { get; set; }
+
+        public static CategoryDataStore Current { get; } = new CategoryDataStore();
+        public CategoryDataStore()
+        {
+            Categories = new List<Category>()
+            {
+               new Category()
+               {
+                   Id=1,
+                   Name = "Pizza"
+               },
+               new Category()
+               {
+                   Id=2,
+                   Name = "Dessert"
+               },
+               new Category()
+               {
+                   Id=3,
+                   Name = "Drink"
+               },
+            };
+        }
+    }
+}
