8054bce54c73cfaad701285b6ea37abf81bbc9ea
Sementiago <Sementiago@DESKTOP-GF1UAA5>
2023-06-21 09:36:15 +0700

    Добавлены классы Category/CategorySize/Ingridient

--- Models/Category.cs
+++ Models/Category.cs
@@ -2,5 +2,9 @@
 {
     public class Category
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Product> Products { get; set; } = new List<Product>();
+        public ICollection<CategorySize> CategorySizes { get; set; } = new List<CategorySize>();
     }
 }
--- Models/CategorySize.cs
+++ Models/CategorySize.cs
@@ -2,5 +2,8 @@
 {
     public class CategorySize
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Capacity { get; set; }
     }
 }
