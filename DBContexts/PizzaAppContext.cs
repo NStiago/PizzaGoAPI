2d795125a926455122aca8b8878f7414255d21a2
Sementiago <Sementiago@DESKTOP-GF1UAA5>
2023-06-24 12:39:07 +0700

    настройка контекста

--- DBContexts/PizzaAppContext.cs
+++ DBContexts/PizzaAppContext.cs
@@ -1,6 +1,17 @@
-﻿namespace PizzaGoAPI.DBContext
+﻿using Microsoft.EntityFrameworkCore;
+using PizzaGoAPI.Entities;
+
+namespace PizzaGoAPI.DBContext
 {
-    public class PizzaAppContext
+    public class PizzaAppContext : DbContext
     {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<CategorySize> CategorySizes { get; set; }
+
+        public PizzaAppContext(DbContextOptions<PizzaAppContext> options) : base(options) 
+        {
+            
+        }
     }
 }
