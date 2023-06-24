07561d7758f364730598954a6c0cfddb27860db6
Sementiago <Sementiago@DESKTOP-GF1UAA5>
2023-06-24 10:59:35 +0700

    Настройка сервиса отправки сообщений

--- Services/MailServece/CloudMailService.cs
+++ Services/MailServece/CloudMailService.cs
@@ -4,6 +4,12 @@
     {
         private readonly string MailTo = string.Empty;
         private readonly string MailFrom = string.Empty;
+
+        public CloudMailService(IConfiguration configuration)
+        {
+            MailTo = configuration["mailSettings:mailToAddress"];
+            MailFrom = configuration["mailSettings:mailFromAddress"];
+        }
         public void Send(string subject, string message)
         {
             Console.WriteLine($"Mail from {MailFrom} to {MailTo}, " +
