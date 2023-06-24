7142a86fd191e1f6e4f51bcc614039f701470653
Sementiago <Sementiago@DESKTOP-GF1UAA5>
2023-06-24 10:53:16 +0700

    Настройка сущностей

--- Entities/Product.cs
+++ Entities/Product.cs
@@ -1,6 +1,19 @@
-﻿namespace PizzaGoAPI.Entities
+﻿using PizzaGoAPI.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PizzaGoAPI.Entities
 {
     public class Product
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? Price { get; set; }
+        public string? Description { get; set; }
+        public string? ImageUrl { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
     }
 }
