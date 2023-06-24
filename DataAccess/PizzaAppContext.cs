12823f68838dc627ce800f9601fda325c0d7f110
Sementiago <Sementiago@DESKTOP-GF1UAA5>
2023-06-24 13:16:28 +0700

    Миграция: добавление начальных данных

--- DataAccess/PizzaAppContext.cs
+++ DataAccess/PizzaAppContext.cs
@@ -8,15 +8,42 @@ namespace PizzaGoAPI.DBContext
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<CategorySize> CategorySizes { get; set; }
+        public DbSet<AddedIngridient> AddedIngridients { get; set; }
 
         public PizzaAppContext(DbContextOptions<PizzaAppContext> options) : base(options)
         {
-            //Database.EnsureCreated();
+
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().HasData(
+                 new { Id = 1, Name = "Пицца" },
+                 new { Id = 2, Name = "Десерт" },
+                 new { Id = 3, Name = "Напиток" }
+                );
+            modelBuilder.Entity<Product>().HasData(
+                new { Id = 1, Name = "Пепперони", Price=200, Description="Some description of peperonni", CategoryId = 1},
+                new { Id = 2, Name = "С ананасами", Price = 250, Description = "Some description of pizza with pineaple", CategoryId = 1},
+                new { Id = 3, Name = "Карбонара", Price = 220, Description = "Some description of carbonara", CategoryId = 1},
+                new { Id = 4, Name = "Шоколадный чизкейк", Price = 50, Description = "Very unusual cake", CategoryId = 2},
+                new { Id = 5, Name = "Молочный коктейль", Price = 30, Description = "Some Cocktail", CategoryId = 3}
+                );
 
+            modelBuilder.Entity<CategorySize>().HasData(
+                new { Id = 1, Name = "Маленькая", Capacity = 25.0, CategoryId = 1 },
+                new { Id = 2, Name = "Средняя", Capacity = 30.0, CategoryId = 1 },
+                new { Id = 3, Name = "Большая", Capacity = 35.0, CategoryId = 1 },
+                new { Id = 4, Name = "Маленький", Capacity = 0.33, CategoryId = 3 },
+                new { Id = 5, Name = "Средний", Capacity = 0.5, CategoryId = 3 },
+                new { Id = 6, Name = "Большой", Capacity = 1.0, CategoryId = 3 }
+                );
 
+            modelBuilder.Entity<AddedIngridient>().HasData(
+                new { Id = 1, Name = "Халапенью", Price = 59 },
+                new { Id = 2, Name = "Сыр", Price = 29 },
+                new { Id = 3, Name = "Кетчуп", Price = 19 }
+                );
         }
     }
 }
---
+++ Migrations/20230624061555_SeedStartedData.cs
@@ -0,0 +1,152 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace PizzaGoAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedStartedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AddedIngridients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AddedIngridients", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "AddedIngridients",
+                columns: new[] { "Id", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, "Халапенью", 59 },
+                    { 2, "Сыр", 29 },
+                    { 3, "Кетчуп", 19 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Пицца" },
+                    { 2, "Десерт" },
+                    { 3, "Напиток" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "CategorySizes",
+                columns: new[] { "Id", "Capacity", "CategoryId", "Name" },
+                values: new object[,]
+                {
+                    { 1, 25.0, 1, "Маленькая" },
+                    { 2, 30.0, 1, "Средняя" },
+                    { 3, 35.0, 1, "Большая" },
+                    { 4, 0.33000000000000002, 3, "Маленький" },
+                    { 5, 0.5, 3, "Средний" },
+                    { 6, 1.0, 3, "Большой" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "Description", "ImageUrl", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, 1, "Some description of peperonni", null, "Пепперони", 200 },
+                    { 2, 1, "Some description of pizza with pineaple", null, "С ананасами", 250 },
+                    { 3, 1, "Some description of carbonara", null, "Карбонара", 220 },
+                    { 4, 2, "Very unusual cake", null, "Шоколадный чизкейк", 50 },
+                    { 5, 3, "Some Cocktail", null, "Молочный коктейль", 30 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AddedIngridients");
+
+            migrationBuilder.DeleteData(
+                table: "CategorySizes",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "CategorySizes",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "CategorySizes",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "CategorySizes",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "CategorySizes",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "CategorySizes",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
