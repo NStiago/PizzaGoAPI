6d17f2ae776281521c692ba86b240e2708a1fcf3
Sementiago <Sementiago@DESKTOP-GF1UAA5>
2023-06-27 11:52:40 +0700

    Добавление NewtonJson

--- Models/CategoryDTOWithoutChild.cs
+++ Models/CategoryDTOWithoutChild.cs
@@ -1,9 +1,13 @@
-﻿namespace PizzaGoAPI.Models
+﻿using PizzaGoAPI.Entities;
+
+namespace PizzaGoAPI.Models
 {
     public class CategoryDTOWithoutChild
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        //public ICollection<Product> Products { get; set; }
+        //public ICollection<CategorySize> CategorySizes { get; set; }
 
     }
 }
