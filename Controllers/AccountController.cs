4184fb88640bd1e1ebf437426a923beb75c8439b
Sementiago <Sementiago@DESKTOP-GF1UAA5>
2023-07-07 17:56:06 +0700

    Создан AccountController( проект авторизации - получение токена)

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using PizzaGoAPI.Services.Authorization;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace PizzaGoAPI.Controllers
 {
@@ -12,6 +16,26 @@ namespace PizzaGoAPI.Controllers
         public AccountController(IOptions<JWTSettings> optionsAccess)
         {
             _options = optionsAccess.Value;
+        }
+        [HttpGet]
+        public IActionResult GetToken()
+        {
+            var claims = new List<Claim>();
+            claims.Add(new Claim(ClaimTypes.Name, "testUser"));
+            claims.Add(new Claim("skillsOfProgramming","junior"));
+            claims.Add(new Claim(ClaimTypes.Role, "Admin"));
+
+            var singingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_options.SecretKey));
+
+            var jwt = new JwtSecurityToken(
+                issuer: _options.Issuer,
+                audience: _options.Audience,
+                claims: claims,
+                expires: DateTime.UtcNow.Add(TimeSpan.FromMinutes(2)),
+                notBefore: DateTime.UtcNow,
+                signingCredentials: new SigningCredentials(singingKey,SecurityAlgorithms.HmacSha256)
+                );
+            return Ok((new JwtSecurityTokenHandler().WriteToken(jwt)));
         }
     }
 }
